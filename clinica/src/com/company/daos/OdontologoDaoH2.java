a1cd291d6ab52ab00976907302ffef3552cf7f8e
Miguel Arias <arias.miguelm@gmail.com>
2022-08-20 19:24:25 -0500

    CRUD DATA H2

--- clinica/src/com/company/daos/OdontologoDaoH2.java
+++ clinica/src/com/company/daos/OdontologoDaoH2.java
@@ -2,10 +2,7 @@ package com.company.daos;
 
 import com.company.model.Odontologo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class OdontologoDaoH2 implements IDao<Odontologo> {
@@ -43,11 +40,56 @@ public class OdontologoDaoH2 implements IDao<Odontologo> {
 
     @Override
     public void eliminar(int numeroMatricula) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+
+            Class.forName(DB_JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            preparedStatement = connection.prepareStatement("DELETE FROM ODONTOLOGO WHERE NUMEROMATRICULA=?");
+            preparedStatement.setLong(1, numeroMatricula);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
 
     }
 
     @Override
     public Odontologo buscar(int numeroMatricula) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+
+            Class.forName(DB_JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            preparedStatement = connection.prepareStatement("SELECT * FROM ODONTOLOGO WHERE NUMEROMATRICULA=?");
+            preparedStatement.setLong(1, numeroMatricula);
+
+
+            ResultSet result = preparedStatement.executeQuery();
+
+            while(result.next()){
+                int numeromatricula = result.getInt("numeromatricula");
+                String nombre = result.getString("nombre");
+                String apellido = result.getString("apellido");
+            }
+            preparedStatement.close();
+
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return null;
     }
 
